27b6a8f2d30b0b8e88b1b472e028735ed9fa9a0d
nhussein11 <nicolashussein14@gmail.com>
2025-05-16 12:19:23 -0300

    fix: initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,36 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@parity/hardhat-polkadot");
+
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+  resolc: {
+    compilerSource: "npm",
+  },
+  networks: {
+    hardhat: {
+      polkavm: true,
+      nodeConfig: {
+        nodeBinaryPath: "../bin/substrate-node",
+        rpcPort: 8000,
+        dev: true,
+      },
+      adapterConfig: {
+        adapterBinaryPath: "../bin/eth-rpc",
+        dev: true,
+      },
+    },
+    localNode: {
+      polkavm: true,
+      url: `http://127.0.0.1:8545`,
+      accounts: [process.env.LOCAL_PRIV_KEY, process.env.AH_PRIV_KEY],
+    },
+    westendHub: {
+      polkavm: true,
+      url: "https://westend-asset-hub-eth-rpc.polkadot.io",
+      accounts: [process.env.AH_PRIV_KEY, process.env.LOCAL_PRIV_KEY],
+    },
+  },
+};
