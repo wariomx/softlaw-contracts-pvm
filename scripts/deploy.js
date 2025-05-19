fe03476f92cf30e36e634635e38845a8272e23ae
nhussein11 <nicolashussein14@gmail.com>
2025-05-19 14:52:33 -0300

    fix: deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,25 +5,26 @@ async function deploy() {
   deployerAddress = account.address;
   console.log(`Deploying contracts using ${deployerAddress}`);
 
-  const eth = await ethers.getContractFactory("UniswapV2ERC20");
-  const ethInstance = await eth.deploy();
-  await ethInstance.waitForDeployment();
-
-  console.log(`ETH deployed to : ${ethInstance.address}`);
+  // Deploy ERC20
+  console.log("Deploying UniswapV2ERC20...");
+  const uniswapV2ERC20 = await ethers.getContractFactory("UniswapV2ERC20");
+  const uniswapV2ERC20Instance = await uniswapV2ERC20.deploy();
+  await uniswapV2ERC20Instance.waitForDeployment();
+  console.log(`ETH deployed to : ${await uniswapV2ERC20Instance.getAddress()}`);
 
   //Deploy Factory
+  console.log("Deploying UniswapV2Factory...");
   const factory = await ethers.getContractFactory("UniswapV2Factory");
   const factoryInstance = await factory.deploy(deployerAddress);
   await factoryInstance.waitForDeployment();
-
-  console.log(`Factory deployed to : ${factoryInstance.address}`);
+  console.log(`Factory deployed to : ${await factoryInstance.getAddress()}`);
 
   // Deploy Pair
+  console.log("Deploying UniswapV2Pair...");
   const pair = await ethers.getContractFactory("UniswapV2Pair");
   const pairInstance = await pair.deploy();
   await pairInstance.waitForDeployment();
-
-  console.log(`Pair deployed to : ${pairInstance.address}`);
+  console.log(`Pair deployed to : ${await pairInstance.getAddress()}`);
 }
 
 deploy()
